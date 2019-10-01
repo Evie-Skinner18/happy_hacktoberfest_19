7f165d0ff30ea6fd03a41aeb8a1d37f5bfcf58df
Joshua <ravioliboy@yahoo.com>
2019-10-01 11:45:17 +0200

    Added styling and animation to Hacktoberfest class/element

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,51 @@ var x = setInterval(function() {
     document.getElementById("countdown").innerHTML = "<h1>End Hacking 2019!</h1>";
   }
 }, 1000);
+
+//Something funky
+//Made this on my break with no real idea/result in mind, I think its a fun little animation.
+//Please feel free to optimize code!
+
+//Get content from element and split every character into array
+const funky = document.querySelector('.Hacktoberfest');
+const content_array = funky.innerHTML.split('');
+funky.innerHTML = '';
+
+let c = 0;
+let b = content_array.length;
+let up = true;
+
+window.setInterval(() => {
+  somethingFunky();
+}, 100);
+
+/**
+ * Go from start to end of array and print characters, and vice versa.
+ */
+function somethingFunky() {
+  if(c < content_array.length && up == true) {
+    funky.innerHTML += content_array[c];
+    c++
+  } else if (c == content_array.length) {
+    up = false;
+  }
+
+  if(up == false) {
+    if(funky.innerHTML.length > 0) {
+      funky.innerHTML = funky.innerHTML.slice(0, -1);
+    } else {
+      up = true;
+      c = 0;
+    }
+  }
+  randomColor();
+}
+
+/**
+ * Change the element's text to a random color
+ */
+function randomColor() {
+  const colors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
+  let random = Math.floor((Math.random() * colors.length) + 1);
+  funky.style.color = colors[random];
+}
