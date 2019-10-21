ed5e5753c401e4b05af914e40af4fa2f11600f05
Georgiou <renos18@hotmail.com>
2019-10-21 16:22:05 +0100

    adding accidentally deleted files

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Happy Hacktoberfest 2019!
+# Happy Hacktoberfest 2019! :jack_o_lantern: :ghost: :fire: :fire:
 
-## This project is a fun little ditty to get creative with. 
-
+:stars: :stars: :stars: :stars: :stars: :stars: :stars: :stars: :stars:
+## Welcome hackers! This project is a beginner-friendly initiative: a fun little ditty to help you get creative with open source.
 ## Feel free to make a new feature/page and submit a pull request.
 
-### This is my frist Hacktoberfest, how do I get started?
+### This is my first Hacktoberfest, how do I get started?  :rocket:
 
-- Have a look for issues labelled as 'good first issue', 'hacktoberfest', or 'beginner'.
+- Have a look for issues labelled as 'good first issue', 'hacktoberfest', 'up-for-grabs', or 'first-timers-only'.
 - Read the instructions in contributing.md
 - Have fun creating something new!
 
+You can view the progress of the website so far here: https://evie-skinner18.github.io/happy_hacktoberfest_19/
+
+## NOTE ##
+Please DO NOT submit random pull requests. First check if an issue already exists for the bugs you found and whether someone else is already working on it. If not, create an issue and assign to yourself.
+
 Happy coding!
+:heart_eyes:
---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+var countDownDate = new Date("Oct 31, 2019 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get today's date and time
+  var now = new Date().getTime();
+
+// Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Display info
+  document.getElementById("days").innerHTML = "<h1 class='countdown'>"+days+" D"+"<h1>";
+  document.getElementById("hours").innerHTML = "<h1 class='countdown'>"+hours+" H"+"<h1>";
+  document.getElementById("min").innerHTML = "<h1 class='countdown'>"+minutes+" M"+"<h1>";
+  document.getElementById("sec").innerHTML = "<h1 class='countdown'>"+seconds+" S"+"<h1>";
+
+// If the count down is over, show the message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "<h2 class='display-1 font-weight-bold'>End Hacking 2019!</h2>";
+  }
+}, 1000);
+
+
+//Something funky
+//Made this on my break with no real idea/result in mind, I think its a fun little animation.
+//Please feel free to optimize code!
+
+//Get content from element and split every character into array
+const funky = document.querySelector('.Hacktoberfest');
+const content_array = funky.innerHTML.split('');
+funky.innerHTML = '';
+
+let c = 0;
+let b = content_array.length;
+let up = true;
+
+window.setInterval(() => {
+  somethingFunky();
+}, 100);
+
+/**
+ * Go from start to end of array and print characters, and vice versa.
+ */
+function somethingFunky() {
+  if(c < content_array.length && up == true) {
+    funky.innerHTML += content_array[c];
+    c++
+  } else if (c == content_array.length) {
+    up = false;
+  }
+
+  if(up == false) {
+    if(funky.innerHTML.length > 0) {
+      funky.innerHTML = funky.innerHTML.slice(0, -1);
+    } else {
+      up = true;
+      c = 0;
+    }
+  }
+  randomColor();
+}
+
+/**
+ * Change the element's text to a random color
+ */
+function randomColor() {
+  const colors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
+  let random = Math.floor((Math.random() * colors.length) + 1);
+  funky.style.color = colors[random];
+}
