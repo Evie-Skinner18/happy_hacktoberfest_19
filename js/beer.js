a51fad731818d87273027a54bb9b52261366333d
Jadson <jadson1197@gmail.com>
2019-10-22 10:04:18 -0300

    Added an effect to the beer

--- js/beer.js
+++ js/beer.js
@@ -3,23 +3,33 @@ window.addEventListener('load', () => {
     const lightBeer = document.getElementById('lightBeer');
     const darkBeer = document.getElementById('darkBeer');
     const liquid = document.getElementById('liquid');
+    const defaultBeer = window.getComputedStyle(liquid).backgroundImage;
 
     const beers = {kriek: '#631526', lightBeer: '#FAAF25', darkBeer: '#400000'};
 
     kriek.addEventListener('click', (e) => {
-        liquid.style.backgroundColor = beers.kriek;
+        liquid.style["background-image"] = defaultBeer.replace("light","kriek");
+        reset(liquid);
         e.preventDefault()
     });
 
     lightBeer.addEventListener('click', (e) => {
-        liquid.style.backgroundColor = beers.lightBeer;
+        liquid.style["background-image"] = defaultBeer;
+        reset(liquid);
         e.preventDefault();
 
     });
 
     darkBeer.addEventListener('click', (e) => {
-        liquid.style.backgroundColor = beers.darkBeer;
+        liquid.style["background-image"] = defaultBeer.replace("light","dark");
+        reset(liquid);
         e.preventDefault();
     })
 
 });
+
+function reset(element){
+    element.style.animation = 'none';
+    element.offsetHeight;
+    element.style.animation = null;
+}
