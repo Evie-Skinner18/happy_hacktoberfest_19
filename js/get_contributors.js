282b41beee10562ed2ae1a3ccaa5249dfeebc02b
Jeff Keilman <jkeilman93@gmail.com>
2019-10-28 20:54:12 -0400

    Remove console.log from contributors page

--- js/get_contributors.js
+++ js/get_contributors.js
@@ -5,7 +5,6 @@ jQuery.contributorList = function (callback) {
 
 jQuery.fn.loadContributors = function () {
     $.contributorList(function (data) {
-        console.log(data)
         var $contributorsListContainer = $('#contributors-list');
         $(data).each(function () {
             $contributorsListContainer.append('<div class="card col-3 m-3"><img class="card-img-top" src=' + this.avatar_url + ' alt=' + this.login + '><div class="card-body text-center"><a href=' + this.html_url + ' class="btn btn-primary">See Profile</a></div>');
