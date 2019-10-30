cf9f6d4755c5ce9634ce9d26e614f6f1d23fff3b
Johnathan Tran <johnathanphuctran21@gmail.com>
2019-10-30 09:26:57 -0700

    Add lantern toggle off and on

---
+++ js/halloween.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    const jackolantern = document.getElementById('jackolantern');
+    var offImg= "../images/off.jpg";
+    var onImg= "../images/on.jpg";
+
+    jackolantern.addEventListener('click', (e) => {
+        if (jackolantern.getAttribute('src') == offImg) {
+            jackolantern.src = onImg;
+        }
+        else {
+            jackolantern.src = offImg;
+        }
+        e.preventDefault()
+    });
+});
