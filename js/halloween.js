d5ec0434ccf5be7955a7053eb9c87ae8f9bcd4fb
José Victor <victor_rpg05@hotmail.com>
2019-10-31 17:15:51 -0300

    Fixs issue that lantern doesn't toggle on /halloween

--- js/halloween.js
+++ js/halloween.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
     const jackolantern = document.getElementById('jackolantern');
-    var offImg= "../images/off.jpg";
-    var onImg= "../images/on.jpg";
+    var offImg= "./images/off.jpg";
+    var onImg= "./images/on.jpg";
 
     jackolantern.addEventListener('click', (e) => {
         if (jackolantern.getAttribute('src') == offImg) {
